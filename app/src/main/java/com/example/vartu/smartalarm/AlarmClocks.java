7c5bdba61a3fc23af011dda8b6be42057f3e09d8
vartult <varttult@rediffmail.com>
2018-08-12 15:00:33 +0530

    Smart Alarm

---
+++ app/src/main/java/com/example/vartu/smartalarm/AlarmClocks.java
@@ -0,0 +1,13 @@
+package com.example.vartu.smartalarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmClocks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_clocks);
+    }
+}
