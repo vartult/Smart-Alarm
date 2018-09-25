0314bb075e233d98f24d3edb520a04ed67d6da52
vartult <vartult@rediffmail.com>
2018-09-26 03:52:14 +0530

    Set Alarm

--- app/src/main/java/com/example/vartu/smartalarm/AlarmClocks.java
+++ app/src/main/java/com/example/vartu/smartalarm/AlarmClocks.java
@@ -1,6 +1,9 @@
 package com.example.vartu.smartalarm;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,5 +19,7 @@ public class AlarmClocks extends AppCompatActivity {
         Intent i = new Intent(this, MapsActivity.class);
         startActivity(i);
     }
+
+
 
 }
--- app/src/main/java/com/example/vartu/smartalarm/MapsActivity.java
+++ app/src/main/java/com/example/vartu/smartalarm/MapsActivity.java
@@ -1,7 +1,18 @@
 package com.example.vartu.smartalarm;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,16 +23,36 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    public void onRequestPermissionsResult(int requestCode,@NonNull String permissions[], @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        if(requestCode==1){
+            if(grantResults[0]== PackageManager.PERMISSION_GRANTED && grantResults.length>0)
+            {
+                if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                }
+            }
+        }
+    }
+
+
+
     private GoogleMap mMap;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        
     }
 
 
@@ -36,13 +67,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        locationManager=(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener=new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                LatLng jiit = new LatLng(location.getLatitude(),location.getLongitude());
+
+                mMap.addMarker(new MarkerOptions().position(jiit).title("END POINT"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jiit,15));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+
+            if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+            }
+            else{
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+               /* Location xlocation=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                LatLng start = new LatLng(xlocation.getLatitude(),xlocation.getLongitude());
+                mMap.clear();
+
+                mMap.addMarker(new MarkerOptions().position(start).title("START POPINT"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(start,15));*/
+            }
+
+
+
     }
+
+
+
+
 
 
 }
