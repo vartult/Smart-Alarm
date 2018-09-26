9c8ab49fa2c24d7632c34b43ff3383fde577a786
vartult <vartult@rediffmail.com>
2018-09-26 14:42:51 +0530

    Set Alarm

--- app/src/main/java/com/example/vartu/smartalarm/MapsActivity.java
+++ app/src/main/java/com/example/vartu/smartalarm/MapsActivity.java
@@ -1,123 +1,186 @@
 package com.example.vartu.smartalarm;
 
 import android.Manifest;
+
 import android.content.Context;
+
 import android.content.pm.PackageManager;
+
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+
 import android.location.LocationListener;
+
 import android.location.LocationManager;
+
 import android.os.Build;
+
+import android.os.LocaleList;
 import android.support.annotation.NonNull;
+
 import android.support.v4.app.ActivityCompat;
+
 import android.support.v4.app.FragmentActivity;
+
 import android.os.Bundle;
+
 import android.support.v4.content.ContextCompat;
+
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+
 import com.google.android.gms.maps.CameraUpdateFactory;
+
 import com.google.android.gms.maps.GoogleMap;
+
 import com.google.android.gms.maps.OnMapReadyCallback;
+
 import com.google.android.gms.maps.SupportMapFragment;
+
 import com.google.android.gms.maps.model.LatLng;
+
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    public void onRequestPermissionsResult(int requestCode,@NonNull String permissions[], @NonNull int[] grantResults){
-        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
-        if(requestCode==1){
-            if(grantResults[0]== PackageManager.PERMISSION_GRANTED && grantResults.length>0)
-            {
-                if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+    private GoogleMap mMap;
+
+    LocationManager locationManager;
+
+    LocationListener locationListener;
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_maps);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            locationManager.requestLocationUpdates(locationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    mMap.clear();
+                    LatLng start=new LatLng(location.getLatitude(),location.getLongitude());
+                    Geocoder geocoder=new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> addresses =geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                        String str = addresses.get(0).getLocality()+" , ";
+                        str+=addresses.get(0).getCountryName();
+                        mMap.addMarker(new MarkerOptions().position(start).title(str));
+
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(start,17));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
                 }
-            }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            });
+        }
+        else if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+        {
+            locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    mMap.clear();
+                    LatLng start=new LatLng(location.getLatitude(),location.getLongitude());
+                    Geocoder geocoder=new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> addresses =geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                        String str = addresses.get(0).getLocality()+" , ";
+                        str+=addresses.get(0).getCountryName();
+                        mMap.addMarker(new MarkerOptions().position(start).title(str));
+
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(start,17));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            });
         }
     }
 
 
 
-    private GoogleMap mMap;
-    LocationManager locationManager;
-    LocationListener locationListener;
+
 
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
     public void onMapReady(GoogleMap googleMap) {
-
 
         mMap = googleMap;
 
-
-        locationManager=(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        locationListener=new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                LatLng jiit = new LatLng(location.getLatitude(),location.getLongitude());
-
-                mMap.addMarker(new MarkerOptions().position(jiit).title("END POPINT"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jiit,15));
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
+        mMap.clear();
 
 
-            if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
-                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
-            }
-            else{
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-                //Location xlocation=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-                LatLng start = new LatLng(28.6296153,77.367837);
-                mMap.clear();
-                mMap.addMarker(new MarkerOptions().position(start).title("START POPINT"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(start,15));
-            }
+
+
+
 
 
 
     }
-
-
-
-
-
 
 }
